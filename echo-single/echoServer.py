37dfefbac6226f2086ed98e5a9cb3add55e43caf
dmccoystephenson <dmccoystephenson@gmail.com>
2023-04-23 22:24:59 -0600

    Modified launch.json

--- echo-single/echoServer.py
+++ echo-single/echoServer.py
@@ -24,7 +24,7 @@ class EchoServer:
 
 if __name__ == "__main__":
     print(" === Echo Server === ")
-    ip = "127.0.0.1"
+    ip = ""
     port = 36578
     server = EchoServer(ip, port)
     server.listen()
